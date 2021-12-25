16469ac68e98a604e96ce81fdc0aa67ea00e2034
SheauChang <S2354A@corp1.ad1.seb.net>
2021-12-25 23:27:53 +0800

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,11 @@
+// Modules
+const john = 'john';
+const peter = 'peter';
+
+const sayHi=(name) => {
+    console.log(`Hello there ${name}`);
+}
+
+sayHi('Susan');
+sayHi(john);
+sayHi(peter);
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+//local
+const secret = 'sUPER SECRET'
+//share
+const john = 'john';
+const peter = 'peter';
+
+
+module.exports = {john, peter}
